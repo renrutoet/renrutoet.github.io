79de79d89ce14b10c072f0b72ccf98ace85179a7
Teo Turner <teoturner@gmail.com>
2023-04-05 19:55:39 +0100

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,6 @@ function App() {
       <h1>Homepage</h1>
       <main className="card">
         <div>About me</div>
-        <div>Minor change</div>
-        <div>Minor change 2</div>
-        <div>Minor change 3</div>
-        <div>Minor change 4</div>
       </main>
     </div>
   );
