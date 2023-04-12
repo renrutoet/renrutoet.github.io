296a018c9b1e4b8d35dd89502588a09cf0b4d433
Teo Turner <teoturner@gmail.com>
2023-04-12 21:57:13 +0100

    fixed typo

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="h-screen bg-black	">
       <div className="flex h-full justify-center items-center	text-center	">
         <div className="text-white	">
-          <h1 className="text-3xl">Welcome to the my Hompage!</h1>
+          <h1 className="text-3xl">Welcome to the homepage of Teo Turner!</h1>
           <h4 className="text-xl">
             This site is currently under construction, come back soon!
           </h4>
